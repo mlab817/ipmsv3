2f73cdb6a7894c0ae413474c71a81ab40a1d22da
Mark Lester Bolotaolo <mlab817@gmail.com>
2020-07-06 15:19:51 +0800

    added missing s

--- app/Models/FundingSourceFinancial.php
+++ app/Models/FundingSourceFinancial.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relation\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class FundingSourceFinancial extends Model
 {
