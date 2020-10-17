edef609777185e6fa2796fa3c0c790731f2d0ac8
mlab817 <mlab817@gmail.com>
2020-10-17 20:18:37 +0800

    fixed a few things

--- app/Observers/ProjectObserver.php
+++ app/Observers/ProjectObserver.php
@@ -44,17 +44,10 @@ class ProjectObserver
       // set processing status id to 'draft'
       $project->processing_status_id = 1;
 
+      // create uuid
       $uuid = Str::uuid();
 
       $project->uuid = $uuid;
-
-      // ProjectProcessingStatus::create([
-      //     'project_id' => $project->id,
-      //     'processing_status_id' => 1,
-      //     'processed_by' => $project->created_by,
-      //     'remarks' => 'new project',
-      //     'processed_at' => Carbon::now()
-      // ]);
     }
 
     public function updating(Project $project)
@@ -72,7 +65,7 @@ class ProjectObserver
 
     public function deleting(Project $project)
     {
-      $project->deleted_by = auth()->user()->id;
+        $project->deleted_by = auth()->user()->id;
     }
 
     public function forceDeleting(Project $project)
