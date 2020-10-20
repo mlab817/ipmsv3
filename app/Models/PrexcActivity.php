d65ade13748e97ba6895e0bb1d40bad453a0509c
Mark Lester Bolotaolo <mlab817@gmail.com>
2020-10-20 14:52:30 +0800

    Update PrexcActivity.php

--- app/Models/PrexcActivity.php
+++ app/Models/PrexcActivity.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\BelongsTo;
-use Illuminate\Database\Eloquent\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class PrexcActivity extends Model
 {
