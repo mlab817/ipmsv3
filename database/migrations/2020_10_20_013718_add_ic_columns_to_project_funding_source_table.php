975f196fe6e7511472a704f02b50e6083725fde8
Mark Lester Bolotaolo <mlab817@gmail.com>
2020-10-20 09:37:51 +0800

    add ic to fs

---
+++ database/migrations/2020_10_20_013718_add_ic_columns_to_project_funding_source_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIcColumnsToProjectFundingSourceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('project_funding_source', function (Blueprint $table) {
+            $table->decimal('infrastructure_target_2016',14,2)->default(0)->nullable();
+            $table->decimal('infrastructure_target_2017',14,2)->default(0)->nullable();
+            $table->decimal('infrastructure_target_2018',14,2)->default(0)->nullable();
+            $table->decimal('infrastructure_target_2019',14,2)->default(0)->nullable();
+            $table->decimal('infrastructure_target_2020',14,2)->default(0)->nullable();
+            $table->decimal('infrastructure_target_2021',14,2)->default(0)->nullable();
+            $table->decimal('infrastructure_target_2022',14,2)->default(0)->nullable();
+            $table->decimal('infrastructure_target_2023',14,2)->default(0)->nullable();
+            $table->decimal('infrastructure_target_total',14,2)->default(0)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('project_funding_source', function (Blueprint $table) {
+            //
+        });
+    }
+}
