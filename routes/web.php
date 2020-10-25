9b635d458863bc2cb2d698c8e47bb47f725c63c5
Mark Lester Bolotaolo <mlab817@gmail.com>
2020-10-25 08:14:43 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-use Log;
+use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
