d944ed999a7c2826533e14c1f14a3fe54c1e6dd6
mlab817 <mlab817@gmail.com>
2020-10-25 14:07:23 +0800

    Update PrexcSubprogramsQuery.php

--- app/GraphQL/Queries/PrexcSubprogramsQuery.php
+++ app/GraphQL/Queries/PrexcSubprogramsQuery.php
@@ -20,12 +20,10 @@ class PrexcSubprogramsQuery
          if ($user->role && $user->role->name === 'encoder') {
            $ou = $user->operating_unit;
 
-           $prexc_subprograms = $ou->$prexc_subprograms;
+           $prexc_subprograms = $ou->prexc_subprograms;
          } else {
            $prexc_subprograms = PrexcSubprogram::all();
          }
-
-         Log::critical(json_encode($prexc_subprograms));
 
          return $prexc_subprograms;
      }
