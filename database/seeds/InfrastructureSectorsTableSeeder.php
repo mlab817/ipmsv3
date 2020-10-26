ca047565e0c58e7f343c6bb22964d43eb2ecfa7a
Mark Lester Bolotaolo <mlab817@gmail.com>
2020-10-26 17:08:51 +0800

    Update InfrastructureSectorsTableSeeder.php

--- database/seeds/InfrastructureSectorsTableSeeder.php
+++ database/seeds/InfrastructureSectorsTableSeeder.php
@@ -11,6 +11,31 @@ class InfrastructureSectorsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('infrastructure_sectors')->insert([
+          [
+            'id' => 1,
+            'name' => 'Social Infrastructure'
+          ],
+          [
+            'id' => 2,
+            'name' => 'Power - Electrification'
+          ],
+          [
+            'id' => 3,
+            'name' => 'Transportation'
+          ],
+          [
+            'id' => 4,
+            'name' => 'Water Resources'
+          ],
+          [
+            'id' => 5,
+            'name' => 'Information and Communications Technology'
+          ],
+          [
+            'id' => 6,
+            'name' => 'Others'
+          ],
+        ]);
     }
 }
