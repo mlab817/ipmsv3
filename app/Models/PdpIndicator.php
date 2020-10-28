acda75e59ee65702e6cbc97093185b90b9190dc2
Mark Lester Bolotaolo <mlab817@gmail.com>
2020-10-28 11:42:59 +0800

    Update PdpIndicator.php

--- app/Models/PdpIndicator.php
+++ app/Models/PdpIndicator.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class PdpIndicator extends Model
 {
-    //
+    public function projects(): BelongsToMany
+    {
+      return $this->belongsToMany(Project::class,'project_pdp_indicators','pdp_indicator_id','project_id','id','id');
+    }
 }
