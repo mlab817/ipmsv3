bfa2ca1ac729e76b1d053359dead40b17eda8216
Mark Lester Bolotaolo <mlab817@gmail.com>
2020-11-01 22:19:33 +0800

    Update UploadSignedCopy.php

--- app/GraphQL/Mutations/UploadSignedCopy.php
+++ app/GraphQL/Mutations/UploadSignedCopy.php
@@ -38,8 +38,10 @@ class UploadSignedCopy
         $uploadedFile = $file->storePubliclyAs('signed copies', $file_title, 'public');
 //        $uploadedFile = $this->uploadFile($args['signed_copy']);
         $processing_status = ProcessingStatus::where('name','endorsed')->first();
+        $ss = SubmissionStatus::where('name','Endorsed')->first();
 
         $project->processing_status_id = $processing_status->id;
+        $project->submission_status_id = $ss;
         $project->processed_by = $user->id;
         $project->signed_copy = $uploadedFile;
         $project->endorsed = true;
