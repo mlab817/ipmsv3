1e4273459b517131e2af0fb3dc3265078e38a340
mlab817 <mlab817@gmail.com>
2020-11-02 12:10:32 +0800

    Update ProjectPolicy.php

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -68,16 +68,16 @@ class ProjectPolicy
       if ($status == 'Draft') {
          return true;
       } else if ($status == 'Finalized') {
-        Log::info($user->name '. was blocked from updating project');
+        Log::info($user->name . ' was blocked from updating project');
          return false;
       } else if ($status == 'Endorsed') {
          return $user->role && $user->role->name == 'reviewer';
       } else if ($status == 'Validated') {
-         Log::info($user->name '. was blocked from updating project since it is validated.');
+         Log::info($user->name .' was blocked from updating project since it is validated.');
          return false;
       }
       // if all else fails
-      Log::info($user->name '. was blocked from updating project for not having submission status.');
+      Log::info($user->name .' was blocked from updating project for not having submission status.');
 
       return false;
     }
