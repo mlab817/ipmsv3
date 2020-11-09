1b5f69515e1319aa6d6dba1ddd17859191a612e6
mlab817 <mlab817@gmail.com>
2020-11-09 22:12:21 +0800

    added export

--- app/GraphQL/Mutations/ExportForConsolidation.php
+++ app/GraphQL/Mutations/ExportForConsolidation.php
@@ -3,12 +3,10 @@
 namespace App\GraphQL\Mutations;
 
 use App\Exports\PrexcActivityExport;
-use App\Exports\ProgramsExport;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
-class ExportExcelMutation
+class ExportForConsolidation
 {
     /**
      * @param  null  $_
@@ -19,12 +17,11 @@ class ExportExcelMutation
         $link = null;
         $user = $context->user();
 
-       $ou = $user->operating_unit;
-       $pa = $ou->prexc_activities;
-       $filename = 'exports/'. time() . '_export.xlsx';
+        $ou = $user->operating_unit;
+        $pa = $ou->consolidates->banner_programs->prexc_activities;
+        $filename = 'exports/'. time() . '_export.xlsx';
 
-       $excel = (new PrexcActivityExport($pa))->store($filename, 'public');
-//         $excel = (new PrexcActivityExport)->store($filename, 'public');
+        $excel = (new PrexcActivityExport($pa))->store($filename, 'public');
 
         if ($excel) {
             $link = config('app.url') . Storage::url($filename);
