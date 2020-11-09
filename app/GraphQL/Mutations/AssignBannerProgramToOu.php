0a36d42e7432971c3322923d3c05397d8cd0dddb
Mark Lester Bolotaolo <mlab817@gmail.com>
2020-11-09 19:06:03 +0800

    added mutations

---
+++ app/GraphQL/Mutations/AssignBannerProgramToOu.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\BannerProgram;
+use App\Models\OperatingUnit;
+
+class AssignBannerProgramToOu
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // TODO implement the resolver
+        $operating_unit = OperatingUnit::find($args['operating_unit_id']);
+
+        if (! $operating_unit) {
+          return null;
+        }
+
+        $banner_programs = BannerProgram::whereIn('id',$args['operating_unit_id'])->get();
+
+        if (! $banner_programs) {
+          return null;
+        }
+
+        $operating_unit->consolidates()->sync($banner_programs);
+
+        return $operating_unit;
+    }
+}
